94526ca57be826c092796a13c50d88f329d3c526
Eloy <eroncalbaz@educacion.navarra.es>
2024-01-16 19:34:20 +0100

    calses

---
+++ src/Mapa.java
@@ -0,0 +1,13 @@
+public class Mapa {
+    private final int[][] tablero = new int [10][10];
+    private Posicion posTablero;
+    public Mapa(){
+        this.posTablero = ;
+    }
+    public int[][] getTablero(){
+        return tablero;
+    }
+    public Posicion getPosTesoro(){
+        return posTablero;
+    }
+}
