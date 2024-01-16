7ec3f0888955da7d53d839d93a7cf63590b4e4cd
Eloy <eroncalbaz@educacion.navarra.es>
2024-01-16 20:26:49 +0100

    .

--- src/Posicion.java
+++ src/Posicion.java
@@ -1,6 +1,7 @@
 public class Posicion {
     private int coordenadaFila;
     private int coordenadaCol;
+
     public Posicion(int coordenadaFila, int coordenadaCol) {
         this.coordenadaFila = coordenadaFila;
         this.coordenadaCol = coordenadaCol;
